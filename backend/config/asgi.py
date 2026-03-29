7b5e220acfa34264675ab258e43b4d98ca379028
Malgani Sridhar Goud <sridhar1002sri@gmail.com>
2026-03-29 16:25:20 +0530

    updated one

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -3,13 +3,13 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from apps.bookings.routing import websocket_urlpatterns as booking_websocket_urlpatterns
-from apps.equipment.routing import websocket_urlpatterns as equipment_websocket_urlpatterns
-from core.authentication.websocket_auth import ClerkAuthMiddlewareStack
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
+
+from apps.bookings.routing import websocket_urlpatterns as booking_websocket_urlpatterns
+from apps.equipment.routing import websocket_urlpatterns as equipment_websocket_urlpatterns
+from core.authentication.websocket_auth import ClerkAuthMiddlewareStack
 
 application = ProtocolTypeRouter(
     {
